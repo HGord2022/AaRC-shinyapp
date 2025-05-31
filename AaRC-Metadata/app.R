adad09e52b21a45e139968ce6fa52a26fcf0fd84
Harry Gordon <hlmgordon@hotmail.co.uk>
2025-05-31 18:55:29 +0100

    Harry added more filtering and colour options

--- AaRC-Metadata/app.R
+++ AaRC-Metadata/app.R
@@ -18,8 +18,10 @@ ui <- navbarPage(
   title = div(
     style = "display: flex; align-items: center; padding-left: 10px;",
     div(
-      tags$img(src = "AaRC_logo.png", height = "50px", style = "margin-right: 10px;"),
-      tags$div("Metadata", style = "font-size: 14px; text-align: center;")
+      tags$img(src = "AaRC_logo.png", height = "40px", style = "margin-right: 0px;"),
+      tags$div("aDNA Metadata",
+               style = "font-size: 20px; text-align: center; color: black; font-weight: normal;"
+      )
     )
   ),
   
@@ -41,40 +43,74 @@ ui <- navbarPage(
         max-width: 1200px;
         margin: 0 auto;
       }
+      .scroll-container {
+        overflow-x: auto;
+        overflow-y: hidden;
+        width: 100%;
+        padding-bottom: 10px;
+      }
     "))
   ),
   
   tabPanel("Explore",
            div(class = "container-custom",
-               leafletOutput("map", height = 600),
-               br(),
                fluidRow(
                  column(4,
-                        pickerInput("species", "Select Species",
+                        pickerInput("species", "Species",
                                     choices = unique(df$samp_taxon_common),
                                     selected = unique(df$samp_taxon_common),
                                     multiple = TRUE,
-                                    options = list(`actions-box` = TRUE))
-                 ),
-                 column(4,
+                                    options = list(`actions-box` = TRUE)),
+                        
+                        pickerInput("sex", "Sex",
+                                    choices = unique(df$molecular_sex),
+                                    selected = unique(df$molecular_sex),
+                                    multiple = TRUE,
+                                    options = list(`actions-box` = TRUE)),
+                        
+                        pickerInput("platform", "Sequencing Platform",
+                                    choices = unique(df$nuclear_sequencing_platform),
+                                    selected = unique(df$nuclear_sequencing_platform),
+                                    multiple = TRUE,
+                                    options = list(`actions-box` = TRUE)),
+                        
+                        pickerInput("strandedness", "Library Strandedness",
+                                    choices = unique(df$nuclear_lib_strandedness),
+                                    selected = unique(df$nuclear_lib_strandedness),
+                                    multiple = TRUE,
+                                    options = list(`actions-box` = TRUE)),
+                        
                         sliderInput("age", "Sample Age Range (Years BP)",
-                                    min = min(df$sample_age, na.rm = TRUE),
-                                    max = max(df$sample_age, na.rm = TRUE),
-                                    value = c(min(df$sample_age, na.rm = TRUE), max(df$sample_age, na.rm = TRUE)))
-                 ),
-                 column(3,
+                                    min = 0,
+                                    max = 150000,
+                                    value = c(0, 150000)),
+                        
                         sliderInput("coverage", "Nuclear Coverage",
                                     min = 0,
-                                    max = max(df$nuclear_depth_of_coverage, na.rm = TRUE),
-                                    value = c(0, max(df$nuclear_depth_of_coverage, na.rm = TRUE)))
+                                    max = 40,
+                                    value = c(0, 40)),
+                        
+                        selectInput("colorBy", "Colour map points by:",
+                                    choices = c("Species" = "samp_taxon_common",
+                                                "Sex" = "molecular_sex",
+                                                "Age" = "sample_age",
+                                                "Coverage" = "nuclear_depth_of_coverage"),
+                                    selected = "samp_taxon_common"),
+                        
+                        uiOutput("colorLegend")
                  ),
-                 column(1,
-                        br(),
-                        downloadButton("downloadData", "Download CSV")
+                 column(8,
+                        leafletOutput("map", height = 600),
+                        br(), br(),
+                        div(style = "display: flex; justify-content: space-between; align-items: center;",
+                            h4("Your filtered dataset:", style="font-size: 30px"),
+                            downloadButton("downloadData", "Download CSV")
+                        ),
+                        div(class = "scroll-container",
+                            dataTableOutput("table")
+                        )
                  )
-               ),
-               br(),
-               dataTableOutput("table")
+               )
            )
   ),
   
@@ -90,11 +126,12 @@ ui <- navbarPage(
   tabPanel("Curators",
            div(class = "container-custom",
                h2("Curators & Contributors"),
-               p("This database was conceived and curated by the following researchers."),
+               p("This database was curated by the following researchers."),
                tags$ul(
-                 tags$li("Dr Anders Bergstrom – University of East Anglia"),
+                 tags$li("Dr Anders Bergström – University of East Anglia"),
                  tags$li("Dr Kevin Daly – University College Dublin"),
-                 tags$li("Matthias Sherman - Francis Crick Institute")
+                 tags$li("Róisín Ferguson – University of East Anglia"),
+                 tags$li("Matthias Sherman – Francis Crick Institute")
                )
            )
   )
@@ -109,15 +146,12 @@ server <- function(input, output, session) {
         sample_age <= input$age[2],
         nuclear_depth_of_coverage >= input$coverage[1],
         nuclear_depth_of_coverage <= input$coverage[2],
-        samp_taxon_common %in% input$species
+        samp_taxon_common %in% input$species,
+        molecular_sex %in% input$sex,
+        nuclear_sequencing_platform %in% input$platform,
+        nuclear_lib_strandedness %in% input$strandedness
       )
   })
-  
-  # Color palette based on species
-  species_palette <- colorFactor(
-    palette = "Set1",
-    domain = unique(df$samp_taxon_common)
-  )
   
   output$map <- renderLeaflet({
     leaflet() %>%
@@ -128,29 +162,85 @@ server <- function(input, output, session) {
   })
   
   observe({
-    leafletProxy("map", data = filteredData()) %>%
+    data <- filteredData()
+    colorField <- input$colorBy
+    
+    if (colorField %in% c("samp_taxon_common", "molecular_sex")) {
+      color_pal <- colorFactor(palette = "Set1", domain = unique(df[[colorField]]))
+      colors <- color_pal(data[[colorField]])
+    } else {
+      color_pal <- colorNumeric(palette = "viridis", domain = df[[colorField]], na.color = "grey")
+      colors <- color_pal(data[[colorField]])
+    }
+    
+    leafletProxy("map", data = data) %>%
       clearMarkers() %>%
       addCircleMarkers(
         lng = ~longitude, lat = ~latitude,
         popup = ~paste0(
           "<b>Sample:</b> ", samp_name, "<br>",
           "<b>Species:</b> ", samp_taxon_common, "<br>",
+          "<b>Sex:</b> ", molecular_sex, "<br>",
           "<b>Age:</b> ", sample_age, "<br>",
           "<b>Coverage:</b> ", nuclear_depth_of_coverage, "<br>",
           "<b>Site:</b> ", site_name, "<br>",
           "<b>Location:</b> ", geo_loc_name
         ),
         radius = 7,
-        fillColor = ~species_palette(samp_taxon_common),
+        fillColor = colors,
         color = "black",
         weight = 0.5,
         fillOpacity = 1
       )
   })
   
+  output$colorLegend <- renderUI({
+    colorField <- input$colorBy
+    if (colorField %in% c("samp_taxon_common", "molecular_sex")) {
+      vals <- unique(df[[colorField]])
+      pal <- colorFactor("Set1", domain = vals)
+      div(
+        style = "margin-top: 10px;",
+        strong("Legend:"),
+        tags$ul(style = "list-style: none; padding-left: 0;",
+                lapply(vals, function(val) {
+                  tags$li(
+                    tags$span(style = paste0("display:inline-block;width:12px;height:12px;background-color:",
+                                             pal(val), ";margin-right:6px;")),
+                    val
+                  )
+                })
+        )
+      )
+    } else {
+      pal <- colorNumeric("viridis", domain = df[[colorField]], na.color = "grey")
+      minVal <- round(min(df[[colorField]], na.rm = TRUE), 2)
+      maxVal <- round(max(df[[colorField]], na.rm = TRUE), 2)
+      div(
+        style = "margin-top: 10px;",
+        strong("Legend:"),
+        tags$div(style = "display: flex; align-items: center;",
+                 tags$span(paste(minVal), style = "margin-right: 8px;"),
+                 tags$div(style = paste0("height: 10px; width: 150px; background: linear-gradient(to right,",
+                                         paste(sapply(seq(0, 1, length.out = 10),
+                                                      function(x) pal(x * (maxVal - minVal) + minVal)),
+                                               collapse = ","), "); margin: 0 8px;")),
+                 tags$span(paste(maxVal))
+        )
+      )
+    }
+  })
+  
   output$table <- renderDataTable({
-    filteredData() %>%
-      select(samp_name, samp_taxon_common, sample_age, nuclear_depth_of_coverage, site_name, geo_loc_name, latitude, longitude)
+    datatable(
+      filteredData(),
+      options = list(
+        scrollX = TRUE,
+        pageLength = 10,
+        autoWidth = TRUE
+      ),
+      class = "display nowrap"
+    )
   })
   
   output$downloadData <- downloadHandler(
